8ce833f4be33ec7c1f267e40add1756bbec53950
Gerardo Gambarte <gerardo.gambarte94@hotmail.com>
2021-07-30 21:21:34 -0300

    menu e-commerce

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar() {
+    return(<>
+        <NavBar />
+            <h2>Las ofertas de la semana</h2>
+            <h2>Inicio</h2>
+            <h2>Contacto</h2>
+            <h2>Productos</h2>
+        </>);
+}
+
+export {NavBar};
+
