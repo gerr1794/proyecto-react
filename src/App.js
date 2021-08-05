dcff4f6c7857ff5665eb7634f32a078c4ed75c93
Gerardo Gambarte <gerardo.gambarte94@hotmail.com>
2021-08-04 23:16:19 -0300

    desafio 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 export default function App() {
   return (
     <div>
       <NavBar />
+      <ItemListContainer greeting= "¿Como estas?" />
     </div>
   );
 }
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,8 @@
+const ItemListContainer = (props) => {
+
+    //console.log(props)
+
+    return <p>Hola! {props.greeting}</p>
+}
+
+export default ItemListContainer;
