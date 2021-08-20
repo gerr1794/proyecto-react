5a3a5a3922e386a145fcf6d559bc1f1a8afdbe4b
Gerardo Gambarte <gerardo.gambarte94@hotmail.com>
2021-08-20 00:25:11 -0300

    desafio 8

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemCount from './components/ItemCount';
 import ItemDetailContainer from './components/ItemDetailContainer';
-
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 export default function App() {
   return (
+    <BrowserRouter>
     <div>
       <NavBar />
+      <Switch>
+      <Route path="/" exact>
+        <ItemListContainer greeting= "¿Como estas?" />
+      </Route>
+
+      <Route path="/productos" exact>
+        <ItemListContainer greeting= "¿Como estas?" />
+      </Route>
+
+
+
+
       <ItemListContainer greeting= "¿Como estas?" />
       <ItemCount stock="10" initial="1" />
       <ItemDetailContainer />
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import CartWidget from './CartWidget';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   return (
     <>
       <h1>C-Piko!</h1>
-      <h2>Inicio</h2>
-      <h2>Productos</h2>
-      <h2>Promociones</h2>
-      <h2>Contacto</h2>
+      <Link to="/"><h2>Inicio</h2></Link>
+      <Link to="/productos"><h2>Productos</h2></Link>
+      <Link to="/promociones"><h2>Promociones</h2></Link>
+      <Link to="/contacto"><h2>Contacto</h2></Link>
       <CartWidget />
     </>
   );
